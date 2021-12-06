2b77aeb3fb76cdf19f644a6f03f8b64dcbd2d3ca
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-12-06 12:40:57 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "easy_list.h"
+using namespace std;
+
+void enter_vector(vector<int> &mass, int len){
+    int a;
+    for(int i = 0; i < len; i++){
+        cin >> a;
+        mass.push_back(a);
+    }
+}
+void print_vector(const vector<int> &mass){
+    for(int i =0 ; i < mass.size(); i++){
+        cout << mass[i] << endl;
+    }
+}
+
+
+int main()
+{
+    int len1, len2;
+    cin >> len1;
+    vector<int> mass;
+    vector<int> mass2;
+    enter_vector(mass, len1);
+    //itc_even_index_list(mass, mass2);
+    itc_even_parts_list(mass, mass2);
+    print_vector(mass2);
+
+    return 0;
+}
