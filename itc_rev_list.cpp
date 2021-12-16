b315cae08b527d5b9eaf84d8b8b00e97dc23549a
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-12-16 20:23:00 +0300

    Update itc_rev_list.cpp

--- itc_rev_list.cpp
+++ itc_rev_list.cpp
@@ -3,7 +3,7 @@
 void itc_rev_list(vector <int> &mass){
     if(mass.size() != 0){
     int temp;
-    for(int i = 0; i <= mass.size() / 2 - 1 + (mass.size()%2); i++){
+    for(int i = 0; i <= mass.size() / 2; i++){
         temp = mass[i];
         mass[i] = mass[mass.size() - 1 - i];
         mass[mass.size() - 1 - i] = temp;
