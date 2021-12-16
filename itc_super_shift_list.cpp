abff3bb21838ee24b9c6d122c5ede92a7de70d8f
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-12-16 12:44:15 +0300

    Add files via upload

--- itc_super_shift_list.cpp
+++ itc_super_shift_list.cpp
@@ -1,18 +1,14 @@
 #include "easy_list.h"
 
-void itc_super_shift_list(vector <int> &mass, int n){
-    n %= mass.size();
-    int temp;
-    long long ch;
-    for(int i = 0; i < mass.size(); i++){
-        ch = i - n;
-        if(ch < 0){
-            ch = mass.size() + ch;
-        }else if(ch > mass.size() -1){
-            ch = ch  - mass.size();
+void itc_super_shift_list(vector<int> &mass, int n){
+    if(n > 0){
+
+    for(int i = 0; i< n; i++){
+            itc_rshift_list(mass);
         }
-        temp = mass[i];
-        mass[i] = mass[ch];
-        mass[ch] = temp;
+    }else if(n < 0){
+        for(int i = 0; i < mass.size() - n - 1; i++){
+            itc_rshift_list(mass);
+        }
     }
 }
