288f6623e9239038bf15fbeaf0aca0cce166e348
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-12-16 20:53:08 +0300

    Update itc_super_shift_list.cpp

--- itc_super_shift_list.cpp
+++ itc_super_shift_list.cpp
@@ -8,12 +8,13 @@ void lshift(vector<int> &mass){
 void itc_super_shift_list(vector<int> &mass, int n){
     if(n > 0){
 
-    for(int i = 0; i< n; i++){
+    for(int i = 0; i< (n % mass.size()); i++){
             itc_rshift_list(mass);
         }
     }else if(n < 0){
-        for(int i = 0; i < mass.size() - n - 1; i++){
-            itc_rshift_list(mass);
+        n = n * (-1);
+        for(int i = 0; i < (n % mass.size()); i++){
+            lshift(mass);
         }
     }
 }
