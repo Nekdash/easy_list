dd7df1b08a84346e9730c467d990adcd96acf730
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-12-16 20:19:35 +0300

    Update itc_positive_list.cpp

--- itc_positive_list.cpp
+++ itc_positive_list.cpp
@@ -3,10 +3,11 @@
 int itc_positive_list(const vector <int> &mass){
 
     int res = 0;
+    if (mass.size() != 0){
     for(int i = 0; i < mass.size(); i++){
         if(mass[i] >= 0){
             res++;
         }
-    }
+    })
     return res;
 }
